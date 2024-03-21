2b024a4b0926e32849b282b5e5b1f5ccd2e0e460
vicnava14 <94847761+vicnava14@users.noreply.github.com>
2024-03-21 15:37:07 -0400

    add

---
+++ inventario.php
@@ -0,0 +1,15 @@
+<?php
+
+// Manejar la solicitud GET para obtener todos los productos del inventarios
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Lógica para obtener y devolver todos losproductos del inventarios
+}
+
+// Manejar la solicitud POST para crear un nuevo producto del inventario
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Lógica para crear una nueva venta
+}
+
+// Manejar otras solicitudes relacionadas con producto del inventario...
+
+?>
---
+++ ventas.php
@@ -0,0 +1,15 @@
+<?php
+
+// Manejar la solicitud GET para obtener todas las ventas
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Lógica para obtener y devolver todas las ventas
+}
+
+// Manejar la solicitud POST para crear una nueva venta
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Lógica para crear una nueva venta
+}
+
+// Manejar otras solicitudes relacionadas con ventas...
+
+?>
